0991935672cb8ea77f072c08ca907b5c11b0fc3b
Thad Achilles <thadachi@ox-v6.msk.21-school.ru>
2022-03-12 21:22:56 +0300

    game_no_break

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define delayTime 1000000
-#define WIDHT 80
+#define delayTime 100000
+#define WIDHT 81
 #define HIGHT 25
 
 char** create_matrix();
@@ -18,9 +18,13 @@ int main() {
     pole_game = create_matrix();
     buff = create_matrix();
     initial_position(pole_game);
+    // game(pole_game, buff);
+     //new_pole(pole_game, buff);
     while (1) {
+        
         game(pole_game, buff);
         new_pole(pole_game, buff);
+        usleep(delayTime);
     }
     free(pole_game);
     free(buff);
@@ -28,17 +32,18 @@ int main() {
 }
 
 char** create_matrix() {
-    char** matrix = (char**) malloc(WIDHT * sizeof(char*) + WIDHT * HIGHT * sizeof(char));
-    char *ptr = (char *) (matrix + WIDHT);
-    for (int i = 0; i < WIDHT; i++) {
-        matrix[i] = ptr + HIGHT *i;
+    char** matrix = (char**) malloc(HIGHT * sizeof(char*) + WIDHT * HIGHT * sizeof(char));
+    char *ptr = (char *) (matrix + HIGHT);
+    for (int i = 0; i < HIGHT; i++) {
+        matrix[i] = ptr + WIDHT *i;
     }
     return matrix;
 }
 
 void new_pole(char **pole, char **buff) {
-    for (int i = 0; i < WIDHT; i++) {
-        for (int j = 0; j < HIGHT; j++) {
+    printf("\e[H\e{\e{");
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
             pole[i][j] = buff[i][j];
             printf("%c", pole[i][j]);
         }
@@ -48,17 +53,15 @@ void new_pole(char **pole, char **buff) {
 void game(char **pole, char **buff) {
     int sosed = 0;
     //char state = ' ';
-    for (int i = 0; i < WIDHT; i++) {
-        for (int j = 0; j < HIGHT; j++) {
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
             sosed = sosed_count(pole, i, j);
-            if (pole[i][j] == ' ' && sosed == 3) {
+            if (pole[i][j] == '_' && sosed == 3) {
                 buff[i][j] = '*';
-            }
-            if (pole[i][j] == '*' && (sosed == 2 || sosed == 3)) {
-                buff[i][j] = '*';
-            }
-            if (pole[i][j] == '*' && (sosed < 2 || sosed > 3)) {
-                buff[i][j] = ' ';
+            }else if (pole[i][j] == '*' && (sosed < 2 || sosed > 3)) {
+                buff[i][j] = '_';
+            } else {
+                buff[i][j] = pole[i][j];
             }
         }
     }
@@ -70,7 +73,7 @@ int sosed_count(char **pole, int x, int y) {
     for (int i = -1; i < 2; i++) {
         for (int j = -1; j < 2; j++) {
             row = (i + x + HIGHT) % HIGHT;
-            col = (j + y + WIDHT) % WIDHT;
+            col = (j + y + WIDHT - 1) % (WIDHT - 1);
             if (pole[row][col] == '*') {
                 count++;
             }
@@ -78,22 +81,14 @@ int sosed_count(char **pole, int x, int y) {
     }
     return pole[x][y] == '*' ? --count : count;
 }
-//void game(char **pole, int w, int h) {
-    /*
-     for (int time = 1; time == 1;) {
-         growing(pole, w, h);
-         poleDraw(pole, w, h);
-     }
-     */
-//}
 
 void initial_position(char **pole) {
     unsigned char c;
-    for (int i = 0; i < WIDHT; i++) {
-        for (int j = 0; j < HIGHT; j++) {
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
             if (scanf("%c", &c) != EOF) {
                 pole[i][j] = c;
-                printf("%c", pole[i][j]);
+                //printf("%c", pole[i][j]);
             }
         }
     }
