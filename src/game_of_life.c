1442646442af419119f8fd0dd003efcb631914a9
Thad Achilles <thadachi@ox-v6.msk.21-school.ru>
2022-03-12 18:04:47 +0300

    scan_pole

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#define delayTime 1000000
 
-int DrawPlayground(int Width, int Height);
+char** create_matrix(int w, int h);
+void initial_position(char **pole, int w, int h);
+//char **cycle(int h, int w);
+//void game(char **pole, int w, int h);
 
 int main() {
-    int Width = 80;
-    int Height = 25;
-    DrawPlayground(Width, Height);
+    char **p = NULL;
+    int Widht = 87, Hight = 25;
+    p = create_matrix(Widht, Hight);
+    initial_position(p, Widht, Hight);
+    //game(p, poleSizeWidht, poleSizeHight);
+    
+    free(p);
     return 0;
 }
+char** create_matrix(int w, int h) {
+    char** matrix = (char**) malloc(w * sizeof(char*) + w * h * sizeof(char));
+    char *ptr = (char *) (matrix + w);
+    for (int i = 0; i < w; i++) {
+        matrix[i] = ptr + h *i;
+    }
+    return matrix;
+}
 
-int DrawPlayground(int Width, int Height) {
-    printf("\e[H\e{\e{");
-        for (int x = 0; x < Height; x++) {
-            for (int y = 0; y < Width; y++) {
-                if ((x == 0 && (y >= 0 && y < Width)) || (x == Height - 1 && (y >= 0 && y < Width))) {
-                    printf("-");
-                } else if ((y == 0 && (x > 0 && x < Height)) || (y == Width - 1 && (x > 0 && x < Height))) {
-                    printf("|");
-                    
-                } else {
-                    printf(" ");
-                }
+
+//void game(char **pole, int w, int h) {
+    /*
+     for (int time = 1; time == 1;) {
+         growing(pole, w, h);
+         poleDraw(pole, w, h);
+     }
+     */
+//}
+
+void initial_position(char **pole, int w, int h) {
+    unsigned char c;
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            if (scanf("%c", &c) != EOF) {
+                pole[i][j] = c;
+                printf("%c", pole[i][j]);
             }
-            printf("\n");
         }
-        printf("\n");
-    return 0;
+    }
 }
