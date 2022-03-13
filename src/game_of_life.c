d41510188ae5ff0e766e7952e82f71af909b28b5
Antoinette Barrett <antoinet@ga-u6.msk.21-school.ru>
2022-03-13 17:46:03 +0300

    Done

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,118 +1,135 @@
 // gcc -Wall -Werror -Wextra game_of_life.c
-//
-/*
-    Правила:
-    
-    Если у клетки:
-     0 или 1 сосед она живёт.
-     4-8 соседей она умирает.
-     2,3 соседа она растёт.
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
-#define delayTime 1000000
+#define WIDHT 81
+#define HIGHT 25
+#define for_x for (int x = 0; x < 25; ++x)
+#define for_y for (int y = 0; y < 81; ++y)
+#define for_xy for_x for_y
 
-void cycle(int h, int w);
-void poleDraw(void *size, int w, int h);
-void growing(void *size, int w, int h);
-void delay(int number_of_seconds);
 
 
-int main(int resolution, char ** v) {
-    // принимает данные о размере поля сразу с запуском => ./a.out 30 30 => размер поля будет 30 на 30
-    int poleSizeWidht = 0, poleSizeHight = 0;
-    if (resolution > 1) poleSizeWidht = atoi(v[1]);
-    if (resolution > 1) poleSizeWidht = atoi(v[1]);
+char** create_matrix();
+void initial_position(char **pole);
+void new_pole(char **pole, char **buff);
+void game(char **pole, char **buff, char** buff_2, char** buff_3);
+int sosed_count(char **pole, int x, int y);
+void stop_game(int *flag, char **pole, char **buff_3);
 
-    if (0 >= poleSizeWidht) poleSizeWidht = 30;
-    if (0 >= poleSizeHight) poleSizeHight = 30;
+int main(int argc, char* argv[]) {
+    if (argc == 2){
+        int speed = 3;
+        if (*argv[1] > 48 && *argv[1] < 54) speed = *argv[1] - 48;
+        
 
-    cycle(poleSizeHight, poleSizeWidht);
+        char **pole_game = NULL;
+        pole_game = create_matrix();
+        initial_position(pole_game);
 
-    return 0;
-}
-/*
-//  Этот мейн классический - принимает данные через сканф
-int main() {
-    int poleSizeWidht = 30, poleSizeHight = 30;
-    if (scanf("Введите размеры поля для игры:%d %d", &poleSizeWidht, &poleSizeHight) != 2) {
-        printf("Данные введены некоректно, введите ширину и высоту поля корректно.");
+        // if (scanf("Введите скорость работы 1-5: %d", &speed) == 1) {
+
+        char** buff = NULL;
+        char** buff_2 = NULL;
+        char** buff_3 = NULL;
+        buff = create_matrix();
+        buff_2 = create_matrix();
+        buff_3 = create_matrix();
+
+        int  delayTime = 50000 * speed;
+        int flag = 0;
+        for (int round = 0; flag == 0; ++round) {
+            if (round > 2) stop_game(&flag, pole_game, buff_3);
+            game(pole_game, buff, buff_2, buff_3);
+            new_pole(pole_game, buff);
+            usleep(delayTime);
+        }
+        printf("\n%d\n", *argv[1] - 48);
+        printf("%d\n",speed);
+        free(pole_game);
+        free(buff);
+        free(buff_2);
+    } else {
+        printf("ASDawsdasd");
     }
-
-    if (0 >= poleSizeWidht) poleSizeWidht = 30;
-    if (0 >= poleSizeHight) poleSizeHight = 30;
-
-    cycle(poleSizeWidht, poleSizeHight);
-
     return 0;
 }
-*/
-void cycle(int h, int w) {
-    unsigned pole[h][w];
-    for (int x = 0; x < w; x++) {
-        for (int y = 0; y < h; y++) {
-        int a = rand() % 2 + 1;
-        if (a == 1) pole[y][x] = ' ';
-            else
-            pole[y][x] = '*';
+
+char** create_matrix() {
+    char** matrix = malloc(HIGHT * sizeof(char*) + WIDHT * HIGHT * sizeof(char));
+    char *ptr = (char *) (matrix + HIGHT);
+    for (int i = 0; i < HIGHT; i++) {
+        matrix[i] = ptr + WIDHT *i;
+    }
+    return matrix;
+}
+
+void new_pole(char **pole, char **buff) {
+    printf("\e[H\e{\e{");
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
+            pole[i][j] = buff[i][j];
+            printf("%c", pole[i][j]);
         }
     }
-    int time = 1;
-    while (time == 1) {
-        // system("cls");
-        // system("clear");
-        poleDraw(pole, w, h);
-        growing(pole, w, h);
-        // delay(delayTime);
-        usleep(200000);
-    }
 }
 
-void poleDraw(void *size, int w, int h) {
-    
-    int (*pole)[w] = size;
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            if (pole[y][x] == '*'){
-                printf("%c", '*');
+void stop_game(int *flag, char **pole, char **buff_3) {
+    /* for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
+            if (pole[i][j] == '_') {
+                *flag = 1;
+            }
+            }
+    }
+    */
+    int num = 0;
+    for_xy if (pole[x][y] != buff_3[x][y]) ++num;
+    if (0 == num) *flag = 1;
+}
+
+void game(char **pole, char **buff, char** buff_2, char** buff_3) {
+    for_xy buff_3[x][y] = buff_2[x][y];
+    for_xy buff_2[x][y] = pole[x][y];
+
+    int sosed = 0;
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
+            sosed = sosed_count(pole, i, j);
+            if (pole[i][j] == '_' && sosed == 3) {
+                buff[i][j] = '*';
+            }else if (pole[i][j] == '*' && (sosed < 2 || sosed > 3)) {
+                buff[i][j] = '_';
             } else {
-                printf("%c", ' ');
+                buff[i][j] = pole[i][j];
             }
         }
     }
+}
     
-}
-void growing(void *size, int w, int h) {
-    unsigned (*pole)[w] = size;
-    unsigned new[h][w];
-
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            int n = 0;
-            for (int y1 = y - 1; y1 <= y + 1; y1++)
-                for (int x1 = x - 1; x1 <= x + 1; x1++)
-                    if (pole[(y1 + h) % h][(x1 + w) % w])
-                        n++;
-
-            if (pole[y][x]) n--;
-            new[y][x] = (n == 3 || (n == 2 && pole[y][x]));
+int sosed_count(char **pole, int x, int y) {
+    int col, row;
+    int count = 0;
+    for (int i = -1; i < 2; i++) {
+        for (int j = -1; j < 2; j++) {
+            row = (i + x + HIGHT) % HIGHT;
+            col = (j + y + WIDHT - 1) % (WIDHT - 1);
+            if (pole[row][col] == '*') {
+                count++;
             }
-    }
-
-    for (int y = 0; y < h; y++) {
-        for (int x = 0; x < w; x++) {
-            pole[y][x] = new[y][x];
         }
     }
+    return pole[x][y] == '*' ? --count : count;
 }
 
-void delay(int number_of_seconds) {
-    int milli_seconds = 1000 * number_of_seconds;
-    clock_t start_time = clock();
-    while (clock() < start_time + milli_seconds) {
+void initial_position(char **pole) {
+    unsigned char c;
+    for (int i = 0; i < HIGHT; i++) {
+        for (int j = 0; j < WIDHT; j++) {
+            if (scanf("%c", &c) != EOF) {
+                pole[i][j] = c;
+            }
+        }
     }
 }
